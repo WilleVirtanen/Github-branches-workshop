394976174de0d3c2a90593343b6047f7ffd82821
maxygdell <maximilian.ygdell@infotiv.se>
2024-10-29 11:53:16 +0100

    Initial commit

---
+++ Calculator.java
@@ -0,0 +1,12 @@
+// Calculator for main branch
+public class Calculator {
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    public double subtract(double a, double b) {
+        return a - b;
+    }
+
+    // More methods to be added by students in different branches
+}
