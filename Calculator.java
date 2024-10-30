d05e5091f83d9a97fcad0ff7eaa116243c827eb5
WilleVirtanen <virtanen.wille@gmail.com>
2024-10-30 13:45:58 +0100

    message

--- Calculator.java
+++ Calculator.java
@@ -1,12 +1,19 @@
+
+import java.awt.SystemColor;
+
 // Calculator for main branch
 public class Calculator {
-    public double add(double a, double b) {
-        return a + b;
+        public double add(double a, double b) {
+            return a + b;
+        }
+    
+        public double subtract(double a, double b) {
+            return a - b;
+        }
+    System.out.println("wille");
+        public double multiply(double a, double b) {
+            return a * b;
+        }
     }
-
-    public double subtract(double a, double b) {
-        return a - b;
-    }
-
     // More methods to be added by students in different branches
 }
